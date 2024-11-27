2f0c8a3388f040a8c868981990ac6f5a6dd91c24
Home8524 <88764103+Home8524@users.noreply.github.com>
2024-11-27 17:44:30 +0900

    supabase fix 2

--- db/supabase.js
+++ db/supabase.js
@@ -1,9 +1,12 @@
-// Supabase 클라이언트 초기화
-const { createClient } = require('@supabase/supabase-js');
+import { createClient } from '@supabase/supabase-js';
 
-// 환경 변수에서 Supabase URL과 키를 가져옵니다.
+// Vercel 환경 변수에서 Supabase URL과 API Key 가져오기
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_ANON_KEY;
+const supabaseKey = process.env.SUPABASE_KEY;
+
+console.log('Supabase URL:', supabaseUrl);  // 확인을 위해 로그 출력
+console.log('Supabase Key:', supabaseKey);  // 확인을 위해 로그 출력
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
